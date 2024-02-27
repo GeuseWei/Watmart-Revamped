5f5f1ba502cd3e38ca7bfbe97fe8ad59a5e96199
Geuse <geuse@geuses-mbp.home>
2024-02-27 13:02:41 -0500

    Initial commit

---
+++ playground/views.py
@@ -0,0 +1,20 @@
+from django.core.cache import cache
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+import logging
+import requests
+
+logger = logging.getLogger(__name__) 
+
+class HelloView(APIView):
+    def get(self, request):
+        try:
+            logger.info('Calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info('Received the response')
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical('httpbin is offline')
+        return render(request, 'hello.html', {'name': 'Geuse'})
