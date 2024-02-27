e9dd60061f174d0b4cfa15087b57f7ae08eba7a8
Geuse <geuse@geuses-mbp.home>
2024-02-27 14:36:13 -0500

    deploy settings

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -162,11 +162,7 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 2525
+
 DEFAULT_FROM_EMAIL = 'from@watmart.com'
 
 ADMINS = [
@@ -174,23 +170,11 @@ ADMINS = [
 ]
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BEAT_SCHEDULE = {
     'notify_customers': {
         'task': 'playground.tasks.notify_customers',
         'schedule': 5,
         'args': ['Hello World'],
-    }
-}
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/2',
-        'TIMEOUT': 10 * 60,
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
     }
 }
 
--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -13,3 +13,21 @@ DATABASES = {
         'PASSWORD': 'Wzj*370104'
     }
 }
+
+CELERY_BROKER_URL = 'redis://redis:6379/1'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/2',
+        'TIMEOUT': 10 * 60,
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+EMAIL_HOST = 'smtp4dev'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
